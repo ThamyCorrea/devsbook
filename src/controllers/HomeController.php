72a0462802af50cef5d501191dc43bc08690b459
Thamy <thamiris_oliveira2@hotmail.com>
2024-07-25 19:12:51 -0300

    Iniciado ação do  login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -5,16 +5,15 @@ use \core\Controller;
 
 class HomeController extends Controller {
 
+    private $loginUsuario;
+
+    public function __construct(){
+        $this->redirect('/login');
+    }
+
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
     }
 
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
+    
 }
